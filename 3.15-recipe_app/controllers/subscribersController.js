45750d712b979fa238bb1376e94f8fadc21cf60b
Hex <hexmusictheory@protonmail.com>
2023-09-03 21:49:51 +0100

    render data from database query via express

--- 3.15-recipe_app/controllers/subscribersController.js
+++ 3.15-recipe_app/controllers/subscribersController.js
@@ -2,3 +2,12 @@
 
 const mongoose = require('mongoose'),
     Subscriber = require('../models/subscriber');
+
+exports.getAllSubscribers = async (req, res, next) => {
+    try {
+        req.data = await Subscriber.find({});
+        next();
+    } catch(e) {
+        next(e);
+    }
+}
--- 3.15-recipe_app/main.js
+++ 3.15-recipe_app/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const express = require('express'),
+    app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+
 const mongoose = require('mongoose');
 mongoose.connect(
     'mongodb://localhost:27017/recipe_db',
@@ -11,41 +17,14 @@ db.once("open", () => {
 });
 
 const Subscriber = require('./models/subscriber');
+const subscribersController = require('./controllers/subscribersController');
 
-async function run() {
-
-    const subscriber1 = new Subscriber({
-        name: "Jon Wexler",
-        email: "jon@jonwexler.com"
+app.get('/subscribers', subscribersController.getAllSubscribers,
+    (req, res, next) => {
+        console.log(req.data);
+        res.render('subscribers', { subscribers: req.data });
     });
 
-    try {
-        await subscriber1.save();
-    } catch (err) {
-        console.error(err);
-    }
-
-    try {
-        await Subscriber.create({
-            name: "Dylan Cobb",
-            email: "dylancobb92@gmail.com"
-        });
-    } catch (err) {
-        console.error(error);
-    }
-}
-run().catch(console.error);
-
-async function query() {
-    const myQuery = Subscriber.where({
-        name: "Jon Wexler"
-    });
-
-    try {
-       const result = await myQuery.findOne();
-       console.log(result);
-    } catch(err) {
-        console.error(err);
-    }
-};
-query().catch(console.error);
+app.listen(app.get('port'), () => {
+    console.log(`Server running on http://localhost:${ app.get('port') }`);
+});
